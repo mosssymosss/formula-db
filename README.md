15dd856055e9e12853fc2fc9c21a3c8bb65fe199
Anahit <anah.arakelyan@gmail.com>
2024-12-27 17:50:07 +0400

    added initialization script, requirements file, .gitignore, edited README

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # formula-db
 
+Run this command
+    ```pip install -r requirements.txt```
+
+You need to have a .env file that as your postgres configurations, i.e.
+    POSTGRES_USER={your_username}
+    POSTGRES_PASSWORD={your_password}
+    POSTGRES_DB={your_database_name}
+    POSTGRES_HOST=localhost
+    POSTGRES_PORT=5432
+
+Run the init.db file
+    
+
---
+++ init_db.py
@@ -0,0 +1,97 @@
+import psycopg2
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+DB_NAME = os.getenv("POSTGRES_DB")
+DB_USER = os.getenv("POSTGRES_USER")
+DB_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+DB_HOST = os.getenv("POSTGRES_HOST")
+DB_PORT = os.getenv("POSTGRES_PORT")
+
+def create_database():
+    try:
+        connection = psycopg2.connect(
+            dbname="postgres",
+            user=DB_USER,
+            password=DB_PASSWORD,
+            host=DB_HOST,
+            port=DB_PORT
+        )
+        connection.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+        cursor = connection.cursor()
+
+        cursor.execute(f"CREATE DATABASE {DB_NAME} OWNER {DB_USER};")
+        print(f"Database {DB_NAME} created successfully")
+
+        cursor.close()
+        connection.close()
+    except Exception as e:
+        print(f"Error: {e}")
+
+def create_table():
+    try:
+        connection = psycopg2.connect(
+            dbname=DB_NAME,
+            user=DB_USER,
+            password=DB_PASSWORD,
+            host=DB_HOST,
+            port=DB_PORT
+        )
+        cursor = connection.cursor()
+
+        cursor.execute("""
+        CREATE TABLE IF NOT EXISTS Driver (
+            driver_id INT PRIMARY KEY,
+            number INT,
+            name VARCHAR(100),
+            nationality VARCHAR(50),
+            team VARCHAR(50),
+            tier VARCHAR(2),
+            dob DATE
+        );
+        """)
+
+        cursor.execute("""
+        CREATE TABLE Circuit (
+            circuit_id INT PRIMARY KEY,
+            name VARCHAR(100),
+            location VARCHAR(100),
+            length DECIMAL(10, 2),
+            laps INT,
+            lap_record VARCHAR(50)
+        );
+        """)
+
+        cursor.execute("""
+        CREATE TABLE Race (
+            driver_id INT,
+            circuit_id INT,
+            race_date DATE,
+            place INT,
+            points INT,
+            is_fastest_lap BOOLEAN,
+            start_place INT,
+            PRIMARY KEY (driver_id, circuit_id, race_date),
+            FOREIGN KEY (driver_id) REFERENCES Driver(driver_id),
+            FOREIGN KEY (circuit_id) REFERENCES Circuit(circuit_id)
+        );
+        """)
+
+        print("Tables created successfully")
+
+        connection.commit()
+        cursor.close()
+        connection.close()
+                       
+    except Exception as e:
+        print(f"Error: {e}")
+
+
+if __name__ == "__main__":
+    create_database()
+    create_table()
+                
+                      
