d9ba8bd40a6336b6685b660d688cbfdc768e9429
Anahit <anah.arakelyan@gmail.com>
2024-12-29 01:49:36 +0400

    added filter query for driver, get driver by id, circuit by id, race by id

--- app/crud.py
+++ app/crud.py
@@ -13,6 +13,13 @@ def get_drivers(db: Session) -> List[Driver]:
     return db.query(Driver).all()
 
 
+def get_driver_by_id(db: Session, driver_id: int):
+    driver = db.query(Driver).filter(Driver.driver_id == driver_id).first()
+    if not driver:
+        raise HTTPException(status_code=404, detail="Driver not found")
+    return driver
+
+
 def create_driver(db: Session, driver: DriverCreate) -> Driver:
     db_driver = Driver(**driver.model_dump())
     db.add(db_driver)
@@ -26,9 +33,7 @@ def create_driver(db: Session, driver: DriverCreate) -> Driver:
 
 
 def update_driver(db: Session, driver: DriverUpdate, driver_id: int) -> Driver:
-    db_driver = db.query(Driver).filter(Driver.driver_id == driver_id).first()
-    if not db_driver:
-        raise HTTPException(status_code=404, detail="Driver not found")
+    db_driver = get_driver_by_id(db, driver_id)
     
     for key, value in driver.model_dump().items():
         setattr(db_driver, key, value)
@@ -38,13 +43,28 @@ def update_driver(db: Session, driver: DriverUpdate, driver_id: int) -> Driver:
 
 
 def delete_driver(db: Session, driver_id: int) -> Driver:
-    db_driver = db.query(Driver).filter(Driver.driver_id == driver_id).first()
-    if not db_driver:
-        raise HTTPException(status_code=404, detail="Driver not found")
-    
+    db_driver = get_driver_by_id(db, driver_id)
     db.delete(db_driver)
     db.commit()
     return {"detail": f"Driver {driver_id} has been deleted"}
+
+
+def get_drivers_filter(db, nationality: str = None, team: str = None, number: int = None, circuit_id: str = None):
+    query = db.query(Driver)
+
+    if nationality:
+        query = query.filter(Driver.nationality == nationality)
+    if team:
+        query = query.filter(Driver.team == team)
+    if number:
+        query = query.filter(Driver.number == number)
+    if circuit_id:
+        query = query.join(Race).filter(Race.circuit_id == circuit_id)
+
+    drivers = query.all()
+    if not drivers:
+        raise HTTPException(status_code=404, detail="No drivers found")
+    return drivers
 
 '''
 Circuit CRUD
@@ -52,6 +72,12 @@ Circuit CRUD
 
 def get_circuits(db: Session) -> List[Circuit]:
     return db.query(Circuit).all()
+
+def get_circuit_by_id(db: Session, circuit_id: int):
+    circuit = db.query(Circuit).filter(Circuit.circuit_id == circuit_id).first()
+    if not circuit:
+        raise HTTPException(status_code=404, detail="Circuit not found")
+    return circuit
 
 def create_circuit(db: Session, circuit: CircuitCreate) -> Circuit:
     db_circuit = Circuit(**circuit.model_dump())
@@ -65,9 +91,7 @@ def create_circuit(db: Session, circuit: CircuitCreate) -> Circuit:
         raise HTTPException(status_code=400, detail="Circuit already exists or invalid data")
 
 def update_circuit(db: Session, circuit: CircuitUpdate, circuit_id: int) -> Circuit:
-    db_circuit = db.query(Circuit).filter(Circuit.circuit_id == circuit_id).first()
-    if not db_circuit:
-        raise HTTPException(status_code=404, detail="Circuit not found")
+    db_circuit = get_circuit_by_id(db, circuit_id)
     
     for key, value in circuit.model_dump().items():
         setattr(db_circuit, key, value)
@@ -76,10 +100,8 @@ def update_circuit(db: Session, circuit: CircuitUpdate, circuit_id: int) -> Circ
     return db_circuit
 
 def delete_circuit(db: Session, circuit_id: int) -> Circuit:
-    db_circuit = db.query(Circuit).filter(Circuit.circuit_id == circuit_id).first()
-    if not db_circuit:
-        raise HTTPException(status_code=404, detail="Circuit not found")
-    
+    db_circuit = get_circuit_by_id(db, circuit_id)
+
     db.delete(db_circuit)
     db.commit()
     return {"detail": f"Circuit {circuit_id} has been deleted"}
@@ -91,6 +113,15 @@ Race CRUD
 def get_races(db: Session) -> List[Race]:
     return db.query(Race).all()
 
+def get_race_by_ids(db: Session, driver_id: int, circuit_id: int, race_date):
+    race = db.query(Race).filter(
+        Race.driver_id == driver_id,
+        Race.circuit_id == circuit_id,
+        Race.race_date == race_date
+    ).first()
+    if not race:
+        raise HTTPException(status_code=404, detail="Race not found")
+    return race
 
 def create_race(db: Session, race: RaceCreate) -> Race:
     race = Race(**race.model_dump())
@@ -105,9 +136,7 @@ def create_race(db: Session, race: RaceCreate) -> Race:
     
 
 def update_race(db: Session, driver_id: int, circuit_id: int, race_date: str, race: RaceUpdate) -> Race:
-    db_race = db.query(Race).filter(Race.driver_id == driver_id, Race.circuit_id == circuit_id, Race.race_date == race_date).first()
-    if not db_race:
-        raise HTTPException(status_code=404, detail="Race not found")
+    db_race = get_race_by_ids(db, driver_id, circuit_id, race_date)
        
     for key, value in race.model_dump().items:
         setattr(db_race, key, value)
@@ -117,9 +146,7 @@ def update_race(db: Session, driver_id: int, circuit_id: int, race_date: str, ra
 
 
 def delete_race(db: Session, driver_id: int, circuit_id: int, race_date: str) -> Race:
-    db_race = db.query(Race).filter(Race.driver_id == driver_id, Race.circuit_id == circuit_id, Race.race_date == race_date).first()  
-    if not db_race:
-        raise HTTPException(status_code=404, detail="Race not found")
+    db_race = get_race_by_ids(db, driver_id, circuit_id, race_date)
     db.delete(db_race)
     db.commit() 
     return {"detail": f"Race on {race_date} for driver {driver_id} at circuit {circuit_id} has been deleted"}
--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException, Depends
+from fastapi import FastAPI, Depends
 from sqlalchemy.orm import Session
 from app import schemas
 from app.database import Base, engine, get_db
@@ -22,6 +22,10 @@ def create_driver(driver: schemas.DriverCreate, db: Session = Depends(get_db)):
 def get_drivers(db: Session = Depends(get_db)):
     return crud.get_drivers(db)
 
+@app.get("/drivers/{driver_id}", response_model=schemas.DriverResponse)
+def get_driver_by_id(driver_id: int, db: Session = Depends(get_db)):
+    return crud.get_driver_by_id(db=db, driver_id=driver_id)
+
 @app.put("/drivers/{driver_id}", response_model=schemas.DriverResponse)
 def update_driver(driver_id: int, driver: schemas.DriverUpdate, db: Session = Depends(get_db)):
     return crud.update_driver(db, driver, driver_id)
@@ -29,6 +33,10 @@ def update_driver(driver_id: int, driver: schemas.DriverUpdate, db: Session = De
 @app.delete("/drivers/{driver_id}", response_model=schemas.DriverResponse)
 def delete_driver(driver_id: int, db: Session = Depends(get_db)):
     return crud.delete_driver(db, driver_id)
+
+@app.get("/drivers/filters/", response_model=List[schemas.DriverResponse])
+def get_drivers_filter(db: Session = Depends(get_db), nationality: str = None, team: str = None, number: int = None, circuit_id: str = None):
+    return crud.get_drivers_filter(db, nationality, team, number, circuit_id)
 
 
 '''
@@ -42,6 +50,10 @@ def create_circuit(circuit: schemas.CircuitCreate, db: Session = Depends(get_db)
 @app.get("/circuits/", response_model=List[schemas.CircuitResponse])
 def get_circuits(db: Session = Depends(get_db)):
     return crud.get_circuits(db)
+
+@app.get("/circuits/{circuit_id}", response_model=schemas.CircuitResponse)
+def get_circuit_by_id(circuit_id: int, db: Session = Depends(get_db)):
+    return crud.get_circuit_by_id(db, circuit_id)
 
 @app.put("/circuits/{circuit_id}", response_model=schemas.CircuitResponse)
 def update_circuit(circuit_id: int, circuit: schemas.CircuitUpdate, db: Session = Depends(get_db)):
@@ -64,9 +76,13 @@ def create_race(race: schemas.RaceCreate, db: Session = Depends(get_db)):
 def get_races(db: Session = Depends(get_db)):
     return crud.get_races(db)
 
+@app.get("/races/{driver_id}/{circuit_id}/{race_date}", response_model=schemas.RaceResponse)
+def read_race(driver_id: int, circuit_id: int, race_date: str, db: Session = Depends(get_db)):
+    return crud.get_race_by_ids(db, driver_id, circuit_id, race_date)
+
 @app.put("/races/", response_model=schemas.RaceResponse)
 def update_race(driver_id: int, circuit_id: int, race_date: str, race: schemas.RaceUpdate, db: Session = Depends(get_db)):
-    return crud.update(db, driver_id, circuit_id, race_date)
+    return crud.update_race(db, driver_id, circuit_id, race_date)
 
 @app.delete("/races/{driver_id}/{circuit_id}/{race_date}", response_model=schemas.RaceResponse)
 def delete_race(driver_id: int, circuit_id: int, race_date: str, db: Session = Depends(get_db)):
